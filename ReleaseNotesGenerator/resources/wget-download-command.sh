cc07e1e98b9d62b1a26db0d7abe36bcef06f9625
Vitali Prudnikovich <vitali.prudnikovich@apryse.com>
2025-09-02 17:12:37 +0300

    Release notes 9.3

--- ReleaseNotesGenerator/resources/wget-download-command.sh
+++ ReleaseNotesGenerator/resources/wget-download-command.sh
@@ -1,2 +1,2 @@
 ﻿#!/usr/bin/env bash
-wget --mirror --convert-links -l 1 --adjust-extension --page-requisites --no-parent https://kb.itextpdf.com/itext/release-itext-core-9-2-0
+wget --mirror --convert-links -l 1 --adjust-extension --page-requisites --no-parent https://kb.itextpdf.com/itext/release-itext-core-9-3-0
